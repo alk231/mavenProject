b9550924db501fe1c1e812988d55b1eb40443e0c
alok kumar <123632977+alk231@users.noreply.github.com>
2025-05-25 21:25:29 +0530

    Add files via upload

---
+++ src/main/java/com/mvit/App.java
@@ -0,0 +1,14 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int sum = a + b;
+        System.out.println("sum=" + sum);
+        System.out.println("Application executed successfully");
+    }
+}
